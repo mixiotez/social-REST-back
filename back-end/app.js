4e2df820de1257dcfd1a4a35710808374f7730f4
Miguel Tellez <miguel.tellez1997@gmail.com>
2018-12-07 20:55:00 -0600

    Account CRUD API

--- back-end/app.js
+++ back-end/app.js
@@ -8,6 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const passport     = require('./helpers/passport');
+const session      = require('express-session');
 
 
 mongoose
@@ -24,6 +26,16 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
+// Session Setup
+app.use(session({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -31,7 +43,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
@@ -52,7 +63,9 @@ app.locals.title = 'Express';
 
 
 const index = require('./routes/index');
+const auth = require('./routes/auth');
 app.use('/', index);
+app.use('/api/auth', auth);
 
 
 module.exports = app;
