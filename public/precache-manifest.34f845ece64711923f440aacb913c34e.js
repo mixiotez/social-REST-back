bd3657e2640eb555e5b21d35e7cc228775deb3c8
Miguel Tellez <miguel.tellez1997@gmail.com>
2019-01-10 01:59:12 -0600

    Integrated React App

---
+++ public/precache-manifest.34f845ece64711923f440aacb913c34e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ffb27c9ef7dd40d0fa4d",
+    "url": "/static/js/main.ffb27c9e.chunk.js"
+  },
+  {
+    "revision": "6e6c8523c96e89b5c5c5",
+    "url": "/static/js/1.6e6c8523.chunk.js"
+  },
+  {
+    "revision": "ffb27c9ef7dd40d0fa4d",
+    "url": "/static/css/main.2b37ee74.chunk.css"
+  },
+  {
+    "revision": "0f35019ece6b726481f90aca05a0d80b",
+    "url": "/index.html"
+  }
+];
