cb4e300d37a3cb58cb7c5d673af3191d91c8ccce
miguel-tellez <miguel.tellez@globant.com>
2020-03-27 23:47:27 -0600

    Created OAuth Twitter request

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const passport = require('passport');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const signature = require('oauth-signature');
+const axios = require('axios');
 
 router.post('/login', async (req, res) => {
 	const { email, password } = req.body;
@@ -27,13 +28,44 @@ router.post('/logout', (req, res) => {
 });
 
 // TWITTER
-router.get('/twitter', passport.authenticate('twitter'));
+router.get('/twitter', (req, res) => {
+	const url = 'https://api.twitter.com/oauth/request_token',
+	timestamp = Math.round(new Date().getTime() / 1000),
+  parameters = {
+      oauth_consumer_key : process.env.CONSUMER_KEY,
+      oauth_signature_method : 'HMAC-SHA1',
+      oauth_timestamp : timestamp,
+      oauth_nonce : timestamp + 'socialRESTNonce',
+			oauth_version : '1.0',
+			oauth_callback: process.env.ENV === 'DEV' ? 'http://localhost:5000/api/auth/twitter/callback' : 'https://social-rest.herokuapp.com/api/auth/twitter/callback'
+	},
+	oauth_signature = signature.generate('POST', url, parameters, process.env.CONSUMER_KEY_SECRET);
 
-router.get('/twitter/callback',
-	passport.authenticate('twitter', {
-		session: false,
-		failureRedirect: '/'
+	let OAuth = "OAuth ";
+
+	for(property in parameters){
+		OAuth += `${property}="${parameters[property]}",`;
+	}
+
+	OAuth += `oauth_callback="${encodeURIComponent(parameters.oauth_callback)}",oauth_signature="${oauth_signature}"`;
+
+  axios({
+		method: 'POST',
+		url,
+		headers: { 'Authorization': OAuth }
 	})
-);
+	.then(response => {
+		res.status(200);
+		res.redirect(`https://api.twitter.com/oauth/authorize?${response.data}`);
+	})
+	.catch(err => res.status(500).json({err, message: 'Couldn\'t generate Twitter credentials'}));
+});
+
+// router.get('/twitter/callback',
+// 	passport.authenticate('twitter', {
+// 		session: false,
+// 		failureRedirect: '/'
+// 	})
+// );
 
 module.exports = router;
