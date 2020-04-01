e1a85b887728d4cc54515780e216c03803c3adbc
miguel-tellez <miguel.tellez@globant.com>
2020-03-31 22:40:18 -0600

    Removed unused files, routes, and extra spaces

--- app.js
+++ app.js
@@ -44,18 +44,13 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
       
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-
-
 // default value for title local
 app.locals.title = 'Social-REST';
-
-
 
 const index = require('./routes/index');
 const account = require('./routes/account');
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router  = express.Router();
 const SocialNetwork = require("../models/SocialNetwork");
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
-
 router.get('/dashboard', (req, res, next) => {
   SocialNetwork.find()
     .then(socialNetwork => {
